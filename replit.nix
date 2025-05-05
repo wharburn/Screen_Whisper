c06b84d635c739bc72824e99873e1b2cb39c522a
Wayne Harburn <wayne@wharburn.com>
2025-05-05 10:44:21 +0100

    updates

--- replit.nix
+++ replit.nix
@@ -6,14 +6,10 @@
     pkgs.python311Packages.wheel
     pkgs.portaudio
     pkgs.ffmpeg
-    pkgs.alsaLib
+    pkgs.libasound2
     pkgs.python311Packages.pyaudio
     pkgs.python311Packages.aiohttp
     pkgs.python311Packages.python-socketio
-    pkgs.python311Packages.websockets
-    pkgs.python311Packages.python-dotenv
-    pkgs.python311Packages.requests
-    pkgs.python311Packages.jinja2
   ];
   env = {
     PYTHONBIN = "${pkgs.python311}/bin/python3.11";
@@ -21,7 +17,7 @@
     PYTHONPATH = "${pkgs.python311Packages.pip}/lib/python3.11/site-packages";
     LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
       pkgs.portaudio
-      pkgs.alsaLib
+      pkgs.libasound2
     ];
   };
 }
